74e1816348a6ac4c4a7ab39a0bf1b03f55449e61
Keeper <virys2@gmail.com>
2023-04-12 12:39:17 +0300

    Testing pytest

--- tests/test_some.py
+++ tests/test_some.py
@@ -1,6 +1,13 @@
-def test_equal():
-    assert 1 != 1, "Number is not equal"
+import sys; sys.path.append('../PyTest')
+import requests
+from configuration import SERVICE_URL
+from enums.global_enums import GlobalErrorMessages
 
-def test_is_not_equal():
-    assert 1 != 2, "Number is equal"
 
+def test_getting_posts():
+    response = requests.get(url=SERVICE_URL)
+    received_posts = response.json()
+    assert response.status_code == 200, \
+        GlobalErrorMessages.WRONG_STATUS_CODE.value
+    assert len(received_posts) == 3, \
+        GlobalErrorMessages.WRONG_STATUS_CODE.value
