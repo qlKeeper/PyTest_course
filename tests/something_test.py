00564d877c5d14d082b82d5a29065b5d2e52d80c
Keeper <virys2@gmail.com>
2023-06-01 12:11:57 +0300

    Improving object validation and working with jsonschema

--- tests/something_test.py
+++ tests/something_test.py
@@ -1,12 +1,16 @@
 import sys; sys.path.append('../Test_Automation_with_Pytest_and_Python')
 import requests
-from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
 
+from configuration import SERVICE_URL
+
+from src.baseclasses.response import Response
+from src.schemas.post import POST_SCHEMA
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    received_posts = response.json()
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    response.assert_status_code(200).validate(POST_SCHEMA)
 
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+
+
+# [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
